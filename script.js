a11de9d04d156c1f99b657658471b802924e09de
Chandrashekar185 <chanduminny32@gmail.com>
2025-02-06 21:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+
+document.getElementById('votingForm').addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+  // Get input values
+  const name = document.getElementById('name').value.trim();
+  const age = parseInt(document.getElementById('age').value.trim());
+  if (!name || isNaN(age)) {
+    alert('Please enter valid details.');
+    return; 
+  }
+  const ageCheckPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000); 
+  });
+  ageCheckPromise
+    .then((message) => {
+      alert(message); 
+    })
+    .catch((message) => {
+      alert(message);
+    });
+});
